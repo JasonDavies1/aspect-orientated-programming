d35d52bfac16dfdb8af81da94361bca1ec01acea
Jason Davies <jason.davies@healthcaregateway.co.uk>
2020-01-17 14:13:03 +0000

    first commit

---
+++ src/main/java/com/github/jasondavies1/aspectorientatedprogramming/AspectOrientatedProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.github.jasondavies1.aspectorientatedprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspectOrientatedProgrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AspectOrientatedProgrammingApplication.class, args);
+    }
+
+}
